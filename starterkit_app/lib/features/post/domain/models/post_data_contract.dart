8fb478aa6c7ef03ba63cc5dc6fba619d1007b930
Dustin Catap <dustincatap@gmail.com>
2025-10-13 15:06:00 +0800

    Remove unused method

--- starterkit_app/lib/features/post/domain/models/post_data_contract.dart
+++ starterkit_app/lib/features/post/domain/models/post_data_contract.dart
@@ -1,6 +1,5 @@
 import 'package:starterkit_app/core/data/database/app_database.dart';
 import 'package:starterkit_app/core/domain/models/json_serializable_object.dart';
-import 'package:starterkit_app/features/post/domain/models/post_entity.dart';
 import 'package:uuid/uuid.dart';
 
 part 'post_data_contract.g.dart';
@@ -21,15 +20,6 @@ class PostDataContract extends JsonSerializableObject {
 }
 
 extension PostDataContractExtension on PostDataContract {
-  PostEntity toEntity() {
-    return PostEntity(
-      userId: userId,
-      id: id,
-      title: title,
-      body: body,
-    );
-  }
-
   PostDataObject toDataObject() {
     return PostDataObject(
       id: const Uuid().v4(),
